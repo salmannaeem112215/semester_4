29886821f66e8e0fc9910888fbf4d1c06f7ac8a9
salman112215 <salman.naeem.112215@gmail.com>
2023-02-06 23:34:37 +0500

    Added OS lab pics

---
+++ SDA lab/lab_3/severnew/severnew/Blogpost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace severnew
+{
+    public class Blogpost
+    {
+        private string postTitle;
+        private string postDescription;
+        private string postApproved;
+
+        public string PostTitle{ get { return postTitle; } set { postTitle = value; } } 
+        public string PostDescription{ get { return postDescription; } set { postDescription = value; } }
+        public string PostApproved { get { return postApproved; } set { postApproved = value; } }
+
+    }
+}
