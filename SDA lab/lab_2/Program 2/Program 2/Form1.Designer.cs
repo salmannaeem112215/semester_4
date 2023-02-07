817eb037793b87c2d1c9a952e9a229ab3ef798b2
salman112215 <salman.naeem.112215@gmail.com>
2023-02-07 14:24:37 +0500

    Completed lab2 task2

--- SDA lab/lab_2/Program 2/Program 2/Form1.Designer.cs
+++ SDA lab/lab_2/Program 2/Program 2/Form1.Designer.cs
@@ -28,12 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lValidity = new System.Windows.Forms.Label();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lUsername = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtSUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.txtSPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lValidity
+            // 
+            this.lValidity.Location = new System.Drawing.Point(112, 127);
+            this.lValidity.Name = "lValidity";
+            this.lValidity.Size = new System.Drawing.Size(100, 15);
+            this.lValidity.TabIndex = 11;
+            this.lValidity.Text = " ";
+            this.lValidity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(37, 60);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(57, 15);
+            this.lPassword.TabIndex = 10;
+            this.lPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 57);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(131, 23);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(125, 101);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lUsername
+            // 
+            this.lUsername.AutoSize = true;
+            this.lUsername.Location = new System.Drawing.Point(37, 31);
+            this.lUsername.Name = "lUsername";
+            this.lUsername.Size = new System.Drawing.Size(60, 15);
+            this.lUsername.TabIndex = 7;
+            this.lUsername.Text = "Username";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(125, 28);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(131, 23);
+            this.txtUserName.TabIndex = 6;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtUserName_TextChanged);
+            // 
+            // txtSUsername
+            // 
+            this.txtSUsername.Location = new System.Drawing.Point(427, 28);
+            this.txtSUsername.Name = "txtSUsername";
+            this.txtSUsername.Size = new System.Drawing.Size(131, 23);
+            this.txtSUsername.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(331, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Enter Username";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(427, 101);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(75, 23);
+            this.btnSignUp.TabIndex = 14;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // txtSPassword
+            // 
+            this.txtSPassword.Location = new System.Drawing.Point(427, 57);
+            this.txtSPassword.Name = "txtSPassword";
+            this.txtSPassword.Size = new System.Drawing.Size(131, 23);
+            this.txtSPassword.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Enter Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSPassword);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSUsername);
+            this.Controls.Add(this.lValidity);
+            this.Controls.Add(this.lPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lUsername);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lValidity;
+        private Label lPassword;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Label lUsername;
+        private TextBox txtUserName;
+        private TextBox txtSUsername;
+        private Label label3;
+        private Button btnSignUp;
+        private TextBox txtSPassword;
+        private Label label2;
     }
 }
--- SDA lab/lab_2/Program 2/Program 2/Form1.cs
+++ SDA lab/lab_2/Program 2/Program 2/Form1.cs
@@ -6,5 +6,62 @@ namespace Program_2
         {
             InitializeComponent();
         }
+
+        string[] username = new string[10];
+        string[] password = new string[10];
+        int u = 0; // max 10
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            bool found = false;
+
+            for (int i = 0; i < u; i++)
+            {
+                if (username[i] == txtUserName.Text)
+                {
+                    if (password[i] == txtPassword.Text)
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        found = false;
+                    }
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                lValidity.Text = "Is Valid";
+                lValidity.BackColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lValidity.Text = "Is Invalid";
+                lValidity.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            if (u<10 && txtSUsername.Text!="" && txtSPassword.Text!="" )
+            {
+            username[u] = txtSUsername.Text;
+            password[u] = txtSPassword.Text;
+            u++;
+                MessageBox.Show("Succesfully Added");
+            }
+        }
     }
 }
