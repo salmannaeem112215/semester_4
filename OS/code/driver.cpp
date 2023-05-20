76cc6e4e1eb9de74b2548955eed6aa5181b26d0c
salmannaeem112215 <salman.naeem.112215@gmail.com>
2023-05-20 11:21:49 +0500

    Updated

---
+++ OS/code/driver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello \n";
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "hi : " << i << endl;
+    }
+
+    cout << "Out of Loop" << endl;
+}
